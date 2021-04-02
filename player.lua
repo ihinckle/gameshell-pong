21299c332ddc021d39554d9fe907ee606381f89d
isaac hinckley <ihinckle@gmail.com>
2021-04-02 00:28:10 -0600

    day 1

---
+++ player.lua
@@ -0,0 +1,21 @@
+Player = {}
+
+function Player:load()
+    self.x = 15
+    self.y = (love.graphics.getHeight()/2)-25
+    self.width = 10
+    self.height = 50
+    self.speed = 250
+end
+
+function Player:update(dt)
+    if love.keyboard.isDown("up") and self.y > 0 then
+        self.y = self.y-(self.speed*dt)
+    elseif love.keyboard.isDown("down") and self.y < (love.graphics.getHeight()-self.height) then
+        self.y = self.y+(self.speed*dt)
+    end
+end
+
+function Player:draw()
+    love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
+end
